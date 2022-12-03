a36730c1acac67269fd8068e38bc4a51b6a28d04
YOOSUSANG <41133135+YOOSUSANG@users.noreply.github.com>
2022-12-03 23:26:47 +0900

    Update jump.cpp

--- programmers/jump.cpp
+++ programmers/jump.cpp
@@ -14,7 +14,7 @@ int jump(int n) {
     if (n==0)
         return 0;
     else if (n == 1 || n == 2) // 어차피 한칸, 2칸(순간이동)으로 모든 것이 반복됨
-        return 0;
+        return 1;
     else
         return jump(n / 2) + jump(n % 2); //한칸 갈건지 아니면 순간이동할지
 }
